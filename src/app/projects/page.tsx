7548aee0988e57ed346368ed3d740c0b7cc9fdf7
Kenned Ferreira <kennedoliveiratm@gmail.com>
2025-11-21 15:48:38 +0000

    suba ele um pouquinho para não ficar abaixo do menu suspenso (_for eleme

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -229,7 +229,7 @@ export default function ProjectsPage() {
               </div>
           </main>
           
-          <Button asChild className="fixed z-20 bottom-24 right-6 w-16 h-16 rounded-full bg-gradient-to-r from-orange-400 to-pink-500 text-white shadow-lg transition-transform hover:scale-110 active:scale-100">
+          <Button asChild className="fixed z-20 bottom-28 right-6 w-16 h-16 rounded-full bg-gradient-to-r from-orange-400 to-pink-500 text-white shadow-lg transition-transform hover:scale-110 active:scale-100">
             <Link href="/tasks/new">
               <Plus size={32} />
             </Link>
